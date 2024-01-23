94f7b75e7acf24f723cb0cf36d9fe9ca2a272a6c
VINCENZO MESCHI <vincenzomeschi12@gmail.com>
2024-01-23 14:09:45 -0500

    Complete lab 3.1

---
+++ project1/rng.py
@@ -0,0 +1,16 @@
+"""Random Number Generation Project (1)
+
+This module will have many different methods of handling
+random number generation. We will use the Middle Square Method,
+Linear Congruential Generators, Lagged Fibonacci Generators,
+and the Acorn Method. 
+
+Extra Information:
+
+  Author: Vincenzo Meschi
+  Date: 2024-01-22
+  Version: Python 3.10.12 (python3 --version)
+"""
+
+class MiddleSquare:
+
