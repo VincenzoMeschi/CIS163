313146b66477c9cc0843c6a01c056120e2fe5e57
VINCENZO MESCHI <vincenzomeschi12@gmail.com>
2024-02-26 14:44:52 -0500

    Monday examples recursion

---
+++ Sandbox/recursion.py
@@ -0,0 +1,39 @@
+# recursive function to remove vowels from a string
+def devowel(string: str) -> str:
+    def is_vowel(letter: str) -> bool:
+        vowels = ["a", "e", "i", "o", "u"]
+        if letter.lower() in vowels:
+            return True
+        return False
+
+    if len(string) == 1:
+        if is_vowel(string):
+            return ""
+        return string
+
+    if is_vowel(string[0]):
+        return devowel(string[1:])
+    return string[0] + devowel(string[1:])
+
+
+def add_asterisks(string: str) -> str:
+    if len(string) == 1:
+        return string
+
+    return string[0] + "*" + add_asterisks(string[1:])
+
+
+print(add_asterisks("allStar"))
+
+
+def remove_dupe(string: str) -> str:
+    if len(string) == 1:
+        return string
+
+    if string[0] == string[1]:
+        return string[0] + remove_dupe(string[2:])
+
+    return string[0] + remove_dupe(string[1:])
+
+
+print(remove_dupe("allStar"))
